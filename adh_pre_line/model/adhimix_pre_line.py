cd61b48c6fd0df846febd9c1403c6e89aca5ebf2
Agung Arisandi <agungarisandi4@gmail.com>
2017-07-18 16:22:33 +0700

    Add files via upload

--- adh_pre_line/model/adhimix_pre_line.py
+++ adh_pre_line/model/adhimix_pre_line.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 class adhimix_pre_line(models.Model):
 	_name = 'adhimix.pre.line'
@@ -8,6 +9,8 @@ class adhimix_pre_line(models.Model):
 	kapasitas_line = fields.Float(string="Kapasitas Line", required=True)
 	dibuat_oleh = fields.Many2one(comodel_name="res.users", readonly=True, string="Dibuat Oleh", default=lambda self: self._uid)
 	produksi_berjalan = fields.One2many(comodel_name="adhimix.pre.line.active", inverse_name="reference", string="Produksi Berjalan")
+	kapasitas_terisi = fields.Float(string="Kapasitas Terisi", compute="_get_kapasitas_terisi")
+	kapasitas_sisa = fields.Float(string="Kapasitas Sisa", compute="_get_kapasitas_sisa")
 
 	# Sequence
 	@api.model
@@ -15,6 +18,20 @@ class adhimix_pre_line(models.Model):
 		vals['name'] = self.env['ir.sequence'].next_by_code('adhimix.pre.line')
 		
 		return super(adhimix_pre_line, self).create(vals)
+
+	@api.depends('kapasitas_terisi')
+	def _get_kapasitas_terisi(self):
+		kapasitas_terisi = 0
+		for rec in self:
+			for x in rec.produksi_berjalan:        
+				kapasitas_terisi += x.qty_produksi
+			rec.kapasitas_terisi = kapasitas_terisi
+
+	@api.depends('kapasitas_sisa')
+	def _get_kapasitas_sisa(self):
+		kapasitas_sisa = 0
+		for rec in self:
+			rec.kapasitas_sisa = rec.kapasitas_line - rec.kapasitas_terisi
 
 
 class adhimix_pre_line_active(models.Model):
@@ -24,7 +41,12 @@ class adhimix_pre_line_active(models.Model):
 	nomor_mo = fields.Many2one(comodel_name="mrp.production", required=True, string="Nomor MO")
 	qty_produksi = fields.Float(string="Qty Produksi", required=True)
 	tanggal_produksi = fields.Date(string="Tanggal Produksi")
-	status_produksi = fields.Char(string="Status Produksi")
+	status_produksi = fields.Char(string="Status Produksi", compute="_get_status_produksi")
+
+	@api.depends('status_produksi')
+	def _get_status_produksi(self):
+		for rec in self:
+			rec.status_produksi = rec.nomor_mo.state
 
 
 class mrp_production(models.Model):
@@ -34,17 +56,20 @@ class mrp_production(models.Model):
 
 	@api.model
 	def create(self, values):
-		if not values.get('name', False) or values['name'] == _('New'):
-			values['name'] = self.env['ir.sequence'].next_by_code('mrp.production') or _('New')
-		if not values.get('procurement_group_id'):
-			values['procurement_group_id'] = self.env["procurement.group"].create({'name': values['name']}).id
-		production = super(mrp_production, self).create(values)
-		production._generate_moves()
-		self.env["adhimix.pre.line.active"].create({
-													'reference': production.line_produksi.id,
-													'nomor_mo' : production.id,
-													'qty_produksi' : production.product_qty,
-													'tanggal_produksi' : production.date_planned_start,
-													'status_produksi' : production.state
-													}).id
+		if values['product_qty'] < self.env['adhimix.pre.line'].browse(values['line_produksi']).kapasitas_sisa:
+			if values['product_uom_id'] == self.env['adhimix.pre.line'].browse(values['line_produksi']).satuan_barang.id:
+				production = super(mrp_production, self).create(values)				
+				self.env["adhimix.pre.line.active"].create({
+															'reference': production.line_produksi.id,
+															'nomor_mo' : production.id,
+															'qty_produksi' : production.product_qty,
+															'tanggal_produksi' : production.date_planned_start,
+															'status_produksi' : production.state
+															}).id
+			else:
+				raise UserError(_('Satuan Uom harus sama dengan satuan line'))					
+		else :
+			raise UserError(_('Kapasitas sisa line tidak mencukupi'))
 		return production
+
+
