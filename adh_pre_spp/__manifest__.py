b636db138932a0e5bb5805d0aa16e6527603d6f9
Agung Arisandi <agungarisandi4@gmail.com>
2017-07-26 17:32:45 +0700

    Add files via upload

---
+++ adh_pre_spp/__manifest__.py
@@ -0,0 +1,22 @@
+{
+	"name": "Precast - SPP", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"product",
+		"mrp",
+	], 
+	"author": "PT.WITACO - Samuel - Agung - Haerul",
+	"website": "https://www.adhimix.co.id",
+	"category": "Surat Perintah Produksi",
+	"summary": "Divisi Precast PT.Adhimix Indonesia",
+	"description": "Module yang digunakan untuk manajemen Surat Perintah Produksi di Divisi Precast",
+	"data": [
+		"menu.xml",
+		"view/spp_view.xml",
+		"data/ir_sequence.xml",
+	],
+	"installable": True,
+	"auto_install": False,
+	"application" : True,
+}
